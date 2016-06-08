314e245cf28daeed3073013dcc4f2a8ec5512b3a
jp-justice <cpt.j.justice@gmail.com>
2016-06-07 20:44:24 -0400

    Class 3 of 9 for HS Arena

---
+++ MageHSA.py
@@ -0,0 +1,245 @@
+common = {"Flamestrike": 111,
+          "Faceless Summoner": 100,
+          "Fireball": 91,
+          "Bog Creeper": 88,
+          "North Sea Kraken": 87,
+          "Piloted Shredder": 86,
+          "Ethereal Conjurer": 82,
+          "Flamecannon": 80,
+          "Water Elemental": 80,
+          "Frostbolt": 79,
+          "Twilight Flamecaller": 79,
+          "Zombie Chow": 79,
+          "Dark Iron Dwarf": 76,
+          "Haunted Creeper": 76,
+          "Mana Wyrm": 76,
+          "Flame Juggler": 75,
+          "Nerubian Prophet": 74,
+          "Polymorph": 73,
+          "Argent Horserider": 72,
+          "Flame Lance": 72,
+          "Stormwind Champion": 72,
+          "Silver Hand Knight": 71,
+          "Mechanical Yeti": 70,
+          "Amani Berserker": 69,
+          "Frost Elemental": 69,
+          "Harvest Golem": 69,
+          "Sen'jin Shieldmasta": 69,
+          "Spider Tank": 69,
+          "Boulderfist Ogre": 68,
+          "Chillwind Yeti": 68,
+          "Pit Fighter": 68,
+          "Scarlet Crusader": 68,
+          "Shattered Sun Cleric": 68,
+          "Snowchugger": 68,
+          "Kvaldir Raider": 67,
+          "Mad Bomber": 67,
+          "Evil Heckler": 66,
+          "Ogre Brute": 66,
+          "Psych-o-Tron": 66,
+          "Stranglethorn Tiger": 66,
+          "Abusive Sergeant": 65,
+          "Bilefin Tidehunter": 65,
+          "Earthen Ring Farseer": 65,
+          "Huge Toad": 65,
+          "Silver Hand Regent": 64,
+          "Spellbreaker": 64,
+          "Spellslinger": 64,
+          "Aberrant Berserker": 63,
+          "Acolyte of Pain": 63,
+          "Annoy-o-Tron": 63,
+          "Argent Squire": 63,
+          "Dire Wolf Alpha": 63,
+          "Frostwolf Warlord": 63,
+          "Gilblin Stalker": 63,
+          "Loot Hoarder": 63,
+          "Sorcerer's Apprentice": 63,
+          "Arcane Intellect": 62,
+          "Boneguard Lieutenant": 62,
+          "Tomb Spider": 62,
+          "Clockwork Knight": 61,
+          "Cult Master": 61,
+          "Drakonid Crusher": 61,
+          "Fen Creeper": 61,
+          "Stonesplinter Trogg": 61,
+          "Worgen Infiltrator": 61,
+          "Darkscale Healer": 60,
+          "Gnomish Inventor": 60,
+          "Mechwarper": 60,
+          "Force-Tank MAX": 59,
+          "Forgotten Torch": 59,
+          "Polluted Hoarder": 59,
+          "Acidic Swamp Ooze": 58,
+          "Mad Scientist": 58,
+          "Mirror Entity": 58,
+          "Mukla's Champion": 58,
+          "Razorfen Hunter": 58,
+          "Refreshment Vendor": 58,
+          "Spectral Knight": 58,
+          "Spiteful Smith": 58,
+          "Stormwind Knight": 58,
+          "Burly Rockjaw Trogg": 57,
+          "Captured Jormungar": 57,
+          "Clockwork Gnome": 57,
+          "Dalaran Aspirant": 57,
+          "Elven Archer": 57,
+          "Faerie Dragon": 57,
+          "Jeweled Scarab": 57,
+          "Murloc Tidehunter": 57,
+          "Raging Worgen": 57,
+          "Ship's Cannon": 57,
+          "Stormpike Commando": 57,
+          "Bloodsail Raider": 56,
+          "Maiden of the Lake": 56,
+          "River Crocolisk": 56,
+          "Silent Knight": 56,
+          "Bloodfen Raptor": 54,
+          "Dragonkin Sorcerer": 54,
+          "Lost Tallstrider": 54,
+          "Puddlestomper": 54,
+          "Tinkertown Technician": 54,
+          "Venture Co. Mercenary": 54,
+          "Archmage": 53,
+          "Eldritch Horror ": 53,
+          "Jungle Panther": 53,
+          "Lowly Squire": 53,
+          "Micro Machine": 53,
+          "Silvermoon Guardian": 53,
+          "Volcanic Drake": 53,
+          "Wolfrider": 53,
+          "Arcane Explosion": 52,
+          "Infested Tauren ": 52,
+          "Novice Engineer": 52,
+          "Squirming Tentacle": 52,
+          "Arcane Missiles": 51,
+          "Dragonling Mechanic": 51,
+          "Explosive Sheep": 51,
+          "Flesheating Ghoul": 51,
+          "Gadgetzan Jouster": 51,
+          "Gorillabot A-3": 51,
+          "Ironfur Grizzly": 51,
+          "Lance Carrier": 51,
+          "Ogre Magi": 51,
+          "Anubisath Sentinel": 50,
+          "Bluegill Warrior": 50,
+          "Dragonhawk Rider": 50,
+          "Fossilized Devilsaur": 50,
+          "Frigid Snobold": 50,
+          "Gurubashi Berserker": 50,
+          "Lord of the Arena": 50,
+          "Oasis Snapjaw": 50,
+          "Twisted Worgen": 50,
+          "Dragon's Breath": 49,
+          "Ice Rager": 49,
+          "Reckless Rocketeer": 49,
+          "Voodoo Doctor": 49,
+          "War Golem": 49,
+          "Antique Healbot": 48,
+          "Blackwing Corruptor": 48,
+          "Ancient Brewmaster": 47,
+          "Cone of Cold": 47,
+          "Ironforge Rifleman": 47,
+          "Blackwing Technician": 46,
+          "Hungry Dragon": 46,
+          "Cult Apothecary": 45,
+          "Kobold Geomancer": 45,
+          "Mirror Image": 45,
+          "Youthful Brewmaster": 45,
+          "Zealous Initiate": 45,
+          "Duplicate": 44,
+          "Nightblade": 43,
+          "Mogu'shan Warden": 42,
+          "Salty Dog": 42,
+          "Tournament Attendee": 42,
+          "Unstable Ghoul": 42,
+          "Murloc Raider": 41,
+          "Stonetusk Boar": 41,
+          "Dancing Swords": 40,
+          "Faceless Behemoth": 40,
+          "Nerub'ar Weblord": 40,
+          "Southsea Deckhand": 40,
+          "Booty Bay Bodyguard": 39,
+          "Cogmaster": 39,
+          "Raid Leader": 39,
+          "Spawn of N'Zoth ": 39,
+          "Gnomeregan Infantry": 37,
+          "Goldshire Footman": 37,
+          "Frostwolf Grunt": 36,
+          "Tentacle of N'Zoth": 36,
+          "Priestess of Elune": 35,
+          "Undertaker": 34,
+          "Frost Nova": 33,
+          "Tauren Warrior": 33,
+          "Tournament Medic": 33,
+          "Dread Corsair": 32,
+          "Ironbeak Owl": 32,
+          "Core Hound": 30,
+          "Windfury Harpy": 30,
+          "Shieldbearer": 28,
+          "Duskboar": 25,
+          "Ice Lance": 25,
+          "Grotesque Dragonhawk": 23,
+          "Ice Barrier": 23,
+          "Thrallmar Farseer": 20,
+          "Murloc Tinyfin": 19,
+          "Wisp": 18,
+          "Silverback Patriarch": 17,
+          "Magma Rager": 16,
+          "Dalaran Mage": 14,
+          "Evolved Kobold": 13,
+          "Leper Gnome": 13,
+          "Flying Machine": 12,
+          "Am'gam Rager": 11,
+          "Stoneskin Gargoyle": 10,
+          "Young Dragonhawk": 10,
+          "Shatter": 8,
+          "Grimscale Oracle": 5,
+}
+
+rare = {}
+epic = {}
+legend = {}
+
+j = 0
+if j < 31:
+    rarity = input("What rarity did you draw?")
+    if rarity == 'common':
+        j = j+1
+        user_cards = []
+        for i in range(3):
+            user_cards.append(input('Enter the name of your card\n').strip())
+
+        print('Highest value card you entered is:')
+        max_card = max(user_cards, key=lambda x: common.get(x, 0))
+        print(max_card, 'value:', common[max_card])
+    elif rarity == 'rare':
+        j = j+1
+        user_cards = []
+        for i in range(3):
+            user_cards.append(input('Enter the name of your card\n').strip())
+
+        print('Highest value card you entered is:')
+        max_card = max(user_cards, key=lambda x: rare.get(x, 0))
+        print(max_card, 'value:', rare[max_card])
+    elif rarity == 'epic':
+        j = j+1
+        user_cards = []
+        for i in range(3):
+            user_cards.append(input('Enter the name of your card\n').strip())
+
+        print('Highest value card you entered is:')
+        max_card = max(user_cards, key=lambda x: epic.get(x, 0))
+        print(max_card, 'value:', epic[max_card])
+    elif rarity == 'legend':
+        j = j+1
+        user_cards = []
+        for i in range(3):
+            user_cards.append(input('Enter the name of your card\n').strip())
+
+        print('Highest value card you entered is:')
+        max_card = max(user_cards, key=lambda x: legend.get(x, 0))
+        print(max_card, 'value:', legend[max_card])
+    else:
+        print('Please input a valid rarity')
+else:
+    print('Congrats on your arena deck!')
